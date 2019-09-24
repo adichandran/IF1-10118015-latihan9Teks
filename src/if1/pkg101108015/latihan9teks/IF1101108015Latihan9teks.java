46a8643b75f1b53cc15e70678ed9b22f1c151790
adichandran <55467774+adichandran@users.noreply.github.com>
2019-09-24 10:32:41 +0700

    Add files via upload

---
+++ src/if1/pkg101108015/latihan9teks/IF1101108015Latihan9teks.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg101108015.latihan9teks;
+
+/**
+ *
+ * @author 
+ * NAMA                 : Adi Chandra Nugraha
+ * KELAS                : IF-1
+ * NIM                  : 10118015
+ * Deskripsi Program    : Program Ini Berisi Program dengan variabel teks
+ */
+public class IF1101108015Latihan9teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil " + newline + "Char:" + tab + "Berisi" + c);
+    }
+    
+}
